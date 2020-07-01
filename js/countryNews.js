ed773059bfc268f1ad29284786d6147387f5e127
JiteshChowdhury <52406879+JiteshChowdhury@users.noreply.github.com>
2020-07-01 17:19:34 +0530

    Add files via upload

--- js/countryNews.js
+++ js/countryNews.js
@@ -1,12 +1,12 @@
 export function printCountryNews(newsData){
     console.log(newsData);
-    document.getElementById("newsList").children[0].innerHTML = '';
+    document.getElementById("newsList").children[1].innerHTML = '';
     if(newsData.articles.length > 0){
         for (let i = 0; i < newsData.articles.length; i++){
-            document.getElementById("newsList").children[0].innerHTML += '<a href=' + newsData.articles[i].url + ' target="_blank"><ol>' + newsData.articles[i].title + '</ol></a>';
+            document.getElementById("newsList").children[1].innerHTML += '<a href=' + newsData.articles[i].url + ' target="_blank"><ol>' + newsData.articles[i].title + '</ol></a>';
         }
     }
     else{
-        document.getElementById("newsList").children[0].innerHTML += '<ol> We are working hard to get news from this location. </ol>';
+        document.getElementById("newsList").children[1].innerHTML += '<ol> We are working hard to get news from this location. </ol>';
     }   
 }
